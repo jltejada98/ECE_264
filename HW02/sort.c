0bb6d7546bdf56e381ce4c458411e011b84ae0d5
jltejada98 <jltejada98@gmail.com>
2018-08-26 12:02:01 -0400

    Selection Sort Written

--- HW02/sort.c
+++ HW02/sort.c
@@ -30,9 +30,34 @@ void ssort(int * arr, int size)
   // The first level specifies which element in the array
   // The second level finds the smallest element among the unsorted
   // elements
-
   // After finding the smallest element among the unsorted elements,
   // swap it with the element of the index from the first level
+
+  int unsorted_array_boundary = 0; //Stores the index of the last element of the unsorted section of the array.
+  int unsorted_array_index = 0; //Stores the index currently cycled through the unsorted section of array.
+  int minimum_index = 0; //Stores minimum index of unsorted array.
+  int temporary_store = 0; //Stores the value of the minimum 
+
+  for (unsorted_array_boundary = 0; unsorted_array_boundary < size - 1; ++unsorted_array_boundary) //Increments index of sorted array.
+  
+    minimum_index = unsorted_array_boundary; //Sets the current element as minimum.
+
+    for (unsorted_array_index = unsorted_array_boundary + 1; unsorted_array_index < size - 1; ++unsorted_array_index) //Checks if current element is the minimum element in the array.
+    {
+      if(arr[unsorted_array_index] < arr[minimum_index]) //If any other element in the unsorted array is the minumum, set its index to the new minimum index.
+      {
+        minimum_index = unsorted_array_index;
+      }
+    }
+
+    if(minimum_index = unsorted_array_boundary) //Once the minimum element in the unsorted array is found, set its index to the last element in the sorted section of the array.
+    {
+      temporary_store = arr[minimum_index];
+      arr[minimum_index] = arr[unsorted_array_boundary];
+      arr[unsorted_array_boundary] = temporary_store;
+    }
+  }
+
   
   // call checkOrder to see whether this function correctly sorts the
   // elements
