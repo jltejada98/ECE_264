2de1da4222b8c42ac56afea14f483bebdea943de
jltejada98 <tejada@purdue.edu>
2018-08-31 15:15:13 -0400

    Completed HW02

--- HW03/josephus.c
+++ HW03/josephus.c
@@ -19,9 +19,10 @@ void eliminate(int n, int k)
       fprintf(stderr, "malloc fail\n");
       return;
     }
+
   // initialize all elements
 
-  int index = 0; //Iterates through all Indecies to Initialize them.
+  int index = 0; //Iterates through all Indecies to Initialize them to their respective index location.
   for(index = 0; index < n; ++index)
   {
     arr[index] = index;
@@ -30,52 +31,50 @@ void eliminate(int n, int k)
   // mark the eliminated element
   // print the index of the marked element
   // repeat until only one element is unmarked
+
   index = 0; //Used to iterate through indexes in the array.
-  int counter = 0; //Counts up to k elements in the array.
+  int counter = 1; //Counts up to k elements in the array.
   int number_eliminated = 0; //Counts number of eliminated elements.
 
   while(number_eliminated < (n - 1))
   {
-    if(arr[index] == -1)
+    if(arr[index] == -1) //Check if element is eliminated.
     {
-      ++index;
-      if (index > n)
-      {
-        index = 0;
-      }
+      ++index; //Skip from checking.
     }
-    else
+    else //If element is not eliminated
     {
-      if (counter < k)
+      if (counter < k) //If counter is less than k, increment counter and index.
       {
         ++counter;
         ++index;
-        if (index > n)
-        {
-          index = 0;
-        } 
       }
-      else
+      else //Otherwise eliminate element.
       {
         printf("%d\n", arr[index]);
         arr[index] = -1;
         ++number_eliminated;
         ++index;
-        counter = 0;
-
-        if (index > n)
-        {
-          index = 0;
-        }
-        
+        counter = 1;
       }
     }
 
+    if (index >= n) //If Index is greater than number of elements.
+    {
+      index = 0;
+    }
+  
   }
+
+
   // print the last one
-
-
-
+  for(index = 0; index < n; ++index)
+  {
+    if(arr[index] != -1)
+    {
+      printf("%d\n", arr[index]);
+    }
+  }
 
   // release the memory of the array
   free (arr);
