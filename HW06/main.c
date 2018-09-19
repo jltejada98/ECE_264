3d400c4e421e5c3eac2856447efb416a027a3c29
jltejada98 <tejada@purdue.edu>
2018-09-19 13:22:54 -0400

    Added Most Recent Files

--- HW06/main.c
+++ HW06/main.c
@@ -10,29 +10,46 @@
 
 /*-------------------------------MODIFY BELOW THIS LINE-------------------------------*/
 
-#ifdef TEST_MAIN
+//#ifdef TEST_MAIN REMEMBER TO REMOVE COMMENTS TO IFDEF STATEMENTS BEFORE SUBMITTING
 int main(int argc, char * * argv)
 {
-    // input file will be specified from the terminal - test files are made available in inputs folder
+  // input file will be specified from the terminal - test files are made available in inputs folder
+  // check for missing input file/output file, if so, "return EXIT_FAILURE;"
+  if (argc < 2)
+  {
+    return EXIT_FAILURE;
+  }
 
-    // check for missing input file/output file, if so, "return EXIT_FAILURE;"
+  // store the input file name in a variable, from argv[1]. For example, char* in_file = argv[1];
+  // store the output file name in a variable, from argv[2]. For example, char* out_file = argv[2];
+  char *input_file_name = argv[1];
+  char *output_file_name = argv[2];
 
-    // store the input file name in a variable, from argv[1]. For example, char* in_file = argv[1];
-    // store the output file name in a variable, from argv[2]. For example, char* out_file = argv[2];
+  // call numberOfElements(in_file) to obtain the number of elements in the file.
+  int number_elements = numberOfElements(input_file_name);
 
-    // call numberOfElements(in_file) to obtain the number of elements in the file.
+  // allocate memory to store the numbers (initialize an array of structures)
+  // check for malloc fail, if so, "return EXIT_FAILURE;"
+  int *input_array = malloc(number_elements *  sizeof(int));
+  int *output_array = malloc(number_elements * sizeof(int));
 
-    // allocate memory to store the numbers (initialize an array of structures)
-            // check for malloc fail, if so, "return EXIT_FAILURE;"
+  if (input_array == NULL)
+  {
+    return EXIT_FAILURE;
+  }
+  if (output_array == NULL)
+  {
+    return EXIT_FAILURE;
+  }
   
-    //call the fillVector(vector, count, in_file); and store the values from the file in the array of strucures.
-  
+  //call the fillVector(vector, count, in_file); and store the values from the file in the array of strucures.
 
-    //Use qsort() function, after defining the comparator() function.
-            //ENSURE THE COMPARATOR FUNCTION IS USED TO SORT THE ARRAY OF STRUCTURES W.R.T. THE STRUCTURE VARIABLE "x".
-  
 
-    //call the writeFile(vector,count, out_file); to save the sorted vector into the file.
-    return EXIT_SUCCESS;
-}
-#endif
+  //Use qsort() function, after defining the comparator() function.
+  //ENSURE THE COMPARATOR FUNCTION IS USED TO SORT THE ARRAY OF STRUCTURES W.R.T. THE STRUCTURE VARIABLE "x".
+
+
+  //call the writeFile(vector,count, out_file); to save the sorted vector into the file.
+  return EXIT_SUCCESS;
+  }
+//#endif  //REMEMBER TO REMOVE COMMENTS TO IFDEF STATEMENTS BEFORE SUBMITTING
--- HW06/vector.c
+++ HW06/vector.c
@@ -12,41 +12,91 @@
 #ifdef TEST_COMP
 int cmp(const void *a, const void *b) 
 { 
-    //typecast the inputs into Vector * form.
-        //For example: Vector *typecasted_b = (Vector *)b;
-    //compare typecased_a->x and typecasted_b->x, and return appropriate values.
+  //typecast the inputs into Vector * form.
+  //For example: Vector *typecasted_b = (Vector *)b;
+  Vector *typecasted_a = (Vector *)a;
+  Vector *typecasted_b = (Vector *)b;
+  int return_value = 0;
+
+  //compare typecasted_a->x and typecasted_b->x, and return appropriate values.
+  if (typecasted_b->x > typecasted_a->x)
+  {
+    return_value = -1;
+  }
+  else if (typecasted_b->x == typecasted_a->x)
+  {
+    return_value = 0;
+  }
+  else if (typecasted_b->x < typecasted_a->x)
+  {
+    return_value = 1;
+  }
+
+ return return_value;
 } 
 #endif
 
 #ifdef TEST_COUNT
 int numberOfElements(char* in_file_name)
 {
-    //initialize file_pointer, and open file in read mode. 
-        //Please note that we are working with binary files. 
-    
-    //use fseek(file_pointer, 0, SEEK_END); to point to the end of the file.
-    //use long where = ftell(file_pointer); to get the size of the file.
-    //use float numelem = where / sizeof(Vector); to obtain the number of elements
-    //return numelem; to send the number of elements back to main().
+  //initialize file_pointer, and open file in read mode. 
+  //Please note that we are working with binary files. 
+  FILE *file_pointer = fopen(in_file_name,"r");
+
+  //Check for fopen failure
+  if (file_pointer == NULL)
+  {
+    printf("fopen Failure numberOfElements\n");
+    return EXIT_FAILURE;
+  }
+  //use fseek(file_pointer, 0, SEEK_END); to point to the end of the file.
+  fseek(file_pointer,0,SEEK_END);
+
+  //use long where = ftell(file_pointer); to get the size of the file.
+  long file_size = ftell(file_pointer);
+
+  //use float numelem = where / sizeof(Vector); to obtain the number of elements
+  float numelem = file_size / sizeof(Vector);
+
+  //return numelem; to send the number of elements back to main().
+  return numelem;
 }
 #endif
 
 #ifdef TEST_FILL
 void fillVector(Vector* vector, int count, char * in_file_name)
 {
-    // open binary file to read after initializing file_pointer 
-   
-    //How to read the file?
-        //use fread(vector, sizeof(Vector), count, file_pointer)
+  // open binary file to read after initializing file_pointer 
+  FILE *file_pointer = fopen(in_file_name,"r");
+
+  if (file_pointer == NULL)
+  {
+    printf("fopen Failure fillVector\n");
+    return;
+  }
+
+  //How to read the file?
+  //use fread(vector, sizeof(Vector), count, file_pointer)
+  fread(vector, sizeof(Vector), count, file_pointer);
+
 }
 #endif
 
 #ifdef TEST_WRITE
 void writeFile(Vector* vector, int count, char* out_file_name)
 {
-    // open binary file to write after initializing file_pointer
-   
-    //How to write to the file?
-        //use fwrite(vector, sizeof(Vector), count, file_pointer)
+  // open binary file to write after initializing file_pointer
+  FILE * file_pointer = fopen(out_file_name, "w");
+
+  if (file_pointer == NULL)
+  {
+    printf("fopen Failure writeFile\n");
+    return;
+  }
+
+  //How to write to the file?
+  //use fwrite(vector, sizeof(Vector), count, file_pointer)
+  fwrite(vector, sizeof(Vector), count, file_pointer);
+
 }
 #endif
