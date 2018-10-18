fb8f1b109fd15367adc684c200616b23eb63fbf8
jltejada98 <tejada@purdue.edu>
2018-10-18 09:26:28 -0400

    HW11 Progress in main.c and bmpimage.c

--- HW11/bmpimage.c
+++ HW11/bmpimage.c
@@ -4,6 +4,77 @@
 
 int Is_BMPHeader_Valid(BMPHeader* header, FILE *fptr) {
 	// use code from HW10
+  // Make sure this is a BMP file -  magic number
+  if (header->type != 0x4d42)
+  {
+    return FALSE;
+  }
+
+  // skip the two unused reserved fields
+
+  // check the offset from beginning of file to image data
+  // essentially the size of the BMP header
+  // BMP_HEADER_SIZE for this exercise/assignment
+  // printf("header->offset %d\n",header->offset); (What to do with this print statement?)
+  if (header->offset != BMP_HEADER_SIZE)
+  {
+    return FALSE;
+  }
+  
+  // check the DIB header size == DIB_HEADER_SIZE
+  if (header->DIB_header_size != DIB_HEADER_SIZE)
+  {
+    return FALSE;
+  }
+
+  // Make sure there is only one image plane
+  if (header->planes != 1)
+  {
+    return FALSE;
+  }
+
+  // Make sure there is no compression
+  if (header->compression != FALSE)
+  {
+    return FALSE;
+  }
+
+  // skip the test for xresolution, yresolution
+  
+  // ncolours and importantcolours should be 0
+  if (header->ncolours !=0)
+  {
+    return FALSE;
+  }
+
+  if (header->importantcolours != 0)
+  {
+    return FALSE;
+  }
+
+  // Make sure we are getting 24 bits per pixel
+  // only for this assignment
+  if (header->bits != 24)
+  {
+    return FALSE;
+  }
+
+  // extra check for file size, image size
+  // based on bits, width, and height
+  // check out the link: https://en.wikipedia.org/wiki/BMP_file_format#Pixel_storage
+
+  // if (header->imagesize != header->bits * header->width * header->height)
+ //  {
+ //    return FALSE;
+ //  }
+
+ //  if (header->size != header->bits * header->width * header->height + BMP_HEADER_SIZE)
+ //  {
+ //    return FALSE;
+ //  }
+
+  return TRUE;
+
 }
 
 #endif
@@ -11,7 +82,56 @@ int Is_BMPHeader_Valid(BMPHeader* header, FILE *fptr) {
 #ifdef TEST_BMPOPENFILE
 
 BMPImage *BMP_Open(const char *filename) {
-// use code from HW10
+  // use code from HW10
+  //open file
+  //read file
+  FILE *fptr = fopen(filename,"r");
+  
+  //Allocate memory for BMPImage*;
+  BMPImage *bmpImage = (BMPImage *)malloc(sizeof(BMPImage));
+  //check for memory allocation errors
+  if (bmpImage == NULL)
+  {
+    return NULL;
+  }
+  
+  //Read the first 54 bytes of the source into the header
+  int read_size = fread(&(bmpImage->header), sizeof(BMPHeader), 1, fptr);
+  
+  //check if the header is valid
+  if (read_size != 1)
+  {
+    BMP_Free(bmpImage);
+    return NULL;
+  }
+  
+  if (Is_BMPHeader_Valid(&(bmpImage->header), fptr) != TRUE)
+  {
+    BMP_Free(bmpImage);
+    return NULL;
+  }
+
+
+  // Allocate memory for image data
+  bmpImage->data = (unsigned char *)malloc(sizeof(unsigned char)*((int)((bmpImage->header).imagesize)));
+  //check error
+  if (bmpImage->data == NULL)
+  {
+    BMP_Free(bmpImage);
+    return NULL;
+  }
+
+  // read in the image data
+  int read_data = fread(bmpImage->data, sizeof(unsigned char), (int)((bmpImage->header).imagesize), fptr); 
+  //check for error while reading
+  if (read_data != (int)(bmpImage->header).imagesize)
+  {
+    BMP_Free(bmpImage);
+    return NULL;
+  }
+
+  fclose(fptr);
+  return bmpImage;
 }
 #endif
 
@@ -20,6 +140,30 @@ BMPImage *BMP_Open(const char *filename) {
 
 int BMP_Write(const char * outfile, BMPImage* image){
 	// use code from HW10
+  FILE *fptr = NULL;
+  fptr = fopen(outfile, "w");
+  //open file and check for error
+  if (fptr == NULL)
+  {
+    return 0;
+  }
+  //check error for writing
+  if (fwrite(&(image->header), sizeof(BMPHeader),1, fptr) != 1)
+  {
+    fclose(fptr);
+    return 0;
+  }
+
+  // write and check for image data:(fwrite(image->data, sizeof(unsigned char), (image->header).imagesize, fptr) != (image->header).imagesize) 
+
+  if (fwrite(image->data, sizeof(unsigned char), (image->header).imagesize, fptr) != (image->header).imagesize)
+  {
+    fclose(fptr);
+    return 0;
+  }
+  
+  fclose(fptr);
+  return TRUE;
 }
 
 #endif
@@ -31,7 +175,19 @@ int BMP_Write(const char * outfile, BMPImage* image){
  */
 #ifdef TEST_BMPFREEFUNC
 void BMP_Free(BMPImage* image) {
-	//free the data
+  //Used Code from HW10
+  //free image if image is true
+  if(image != NULL)
+  {
+    if (image->data != NULL)
+    {
+      free(image->data);
+    }
+    free(image);
+  }
+
+  return;
+
 }
 
 #endif
--- HW11/hw11.c
+++ HW11/hw11.c
@@ -1,13 +1,48 @@
 #include "hw10.h"
 //Modify this file
-#ifdef TEST_MAIN
+//#ifdef TEST_MAIN
 
 int main(int argc, char **argv){
   // check the arguments - please read readme about validity of arguments
+  if (argc != 5)
+  {
+    printf("Wrong arguments\n");
+    return EXIT_FAILURE;
+  }
+  
+  FILE *input_file_pointer; //File Pointer used to check for correct name.
+  char *input_file_name = argv[1];
+  char *output_file_name = argv[2];
+  char *radius = argv[3];
+  char *epsilon = argv[4];
+
+  //Check First Input
+  input_file_pointer = fopen(input_file_name, "r");
+  if (input_file_pointer == NULL)
+  {
+    printf("Error opening BMP file\n");
+    return EXIT_FAILURE;
+  }
+
   // check radius and epsilon values -  read readme for the validity of argument
+  if ((radius == NULL) | (epsilon == NULL))
+  {
+    printf("Wrong inputs\n");
+    return EXIT_FAILURE;
+  }
+
   // open the BMP file
+   BMPImage *original_image = BMP_Open(input_file_name);
+
   // convert to gray scale
+   BMPImage *gray_image = ImgToGray(original_image);
+
 	// check for error in converting to gray scale
+   if (gray_image == NULL)
+  {
+    return EXIT_FAILURE;
+  }  
+
 
   // call adaptive threshold function
   // check for errors after calling adaptive threshold
