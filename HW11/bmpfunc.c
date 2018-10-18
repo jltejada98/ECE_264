41be330e01564ea8b8b40cf4e063c0c524e27902
jltejada98 <tejada@purdue.edu>
2018-10-18 16:05:51 -0400

    HW11 Progress (Output Image Incorrect)

--- HW11/bmpfunc.c
+++ HW11/bmpfunc.c
@@ -68,7 +68,7 @@ BMPImage * ImgToGray(BMPImage * image){
 #endif
 
 //Write your own code for Adaptive Thresholding Function
-//#ifdef TEST_ADAPTIVETHRESHOLDING
+#ifdef TEST_ADAPTIVETHRESHOLDING
 
 BMPImage * AdaptiveThresholding(BMPImage * grayImage, int radius, int epsilon){
  	// allocate space for the image
@@ -84,11 +84,12 @@ BMPImage * AdaptiveThresholding(BMPImage * grayImage, int radius, int epsilon){
 		return NULL;
 	}
 
-	int pixel_outer_loop=0;
+	int pixel_outer_loop = 0;
+  int pixel_inner_loop = 0;
 	//Run a nested loop for all elements using height and width
-  for (int row = 0; row < (adaptive->header).height; ++row)
+  for (int row = 0; row < (adaptive->header).height; row += 3)
   {
-    for (int col = 0; col < (adaptive->header).width; ++col)
+    for (int col = 0; col < (adaptive->header).width; col +=3)
     {
       //Find the maximum of top row, b)ottom rpw, left column and right column using radius
       int toprow = MAX(0, row-radius);
@@ -97,21 +98,20 @@ BMPImage * AdaptiveThresholding(BMPImage * grayImage, int radius, int epsilon){
       int rightcol = MIN((adaptive->header).width-1, col+radius);
 
       //Calculate the average of all pixels
-      int sum_neighborhood = 0; //Holds the value of the sum.
+      int sum_neighborhood = 0; //Holds the value of the sum of the neighborhood.
       int counter_neighborhood = 0; //Holds the number of pixels in neighborhood being read.
       int average_epsilon = 0; //Holds average value of neighborhood pixels.
-      int pixel_inner_loop = 0;
 
       //Run loop from toprow to bottom row
-      for (int row2 = toprow; row2 < bottomrow; ++row2)
+      for (int row2 = toprow; row2 < bottomrow; row2 +=3) //Increment by 3 or 1?
       {
         //Inside which run a loop from left column to right column
-        for (int col2 = leftcol; col2 < rightcol; ++col2)
+        for (int col2 = leftcol; col2 < rightcol; col2 +=3)
         {
           //calculate the location of each pixel using (row2*width + col2)*3;
-          pixel_inner_loop = (row2*(adaptive->header).width + col2)*3;
+          pixel_inner_loop = (row2*(adaptive->header).width + col2); 
           //add all data values at every location point in data.
-          sum_neighborhood += grayImage->data[pixel_inner_loop +2] + grayImage->data[pixel_inner_loop +1] +grayImage->data[pixel_inner_loop];
+          sum_neighborhood += grayImage->data[pixel_inner_loop];
           //keep a counter for averaging
           ++counter_neighborhood;
         }
@@ -121,8 +121,8 @@ BMPImage * AdaptiveThresholding(BMPImage * grayImage, int radius, int epsilon){
       //calculate average (using int)
       average_epsilon = sum_neighborhood / counter_neighborhood;
       //check if average_epsilon is greater than grayImage->data[at that pixel point]
-      int pixel_outer_loop = (row*(adaptive->header).width + col)*3;
-      if (average_epsilon > grayImage->data[pixel_outer_loop] )
+      pixel_outer_loop = (row*(adaptive->header).width + col);
+      if (grayImage->data[pixel_outer_loop] < (average_epsilon - epsilon))
       {
         //then assign adaptive thresholding image data as 0(black) for data value at pixel, pixel+1, pixel+2
         grayImage->data[pixel_outer_loop + 2] = 0;
@@ -141,6 +141,5 @@ BMPImage * AdaptiveThresholding(BMPImage * grayImage, int radius, int epsilon){
   //return the image after adaptive thresholding
   return(adaptive);
 }
-
 
 #endif
--- HW11/hw11.c
+++ HW11/hw11.c
@@ -1,7 +1,7 @@
-#include "hw10.h"
+#include "hw11.h"
 //Modify this file
 
-//#ifdef TEST_MAIN
+#ifdef TEST_MAIN
 
 int main(int argc, char **argv){
   // check the arguments - please read readme about validity of arguments
@@ -14,8 +14,8 @@ int main(int argc, char **argv){
   FILE *input_file_pointer; //File Pointer used to check for correct name.
   char *input_file_name = argv[1];
   char *output_file_name = argv[2];
-  char *radius = argv[3];
-  char *epsilon = argv[4];
+  int radius = (int) strtol(argv[3],NULL,10);
+  int epsilon = (int) strtol(argv[4],NULL, 10);
 
   //Check First Input
   input_file_pointer = fopen(input_file_name, "r");
@@ -25,8 +25,8 @@ int main(int argc, char **argv){
     return EXIT_FAILURE;
   }
 
-  // check radius and epsilon values -  read readme for the validity of argument
-  if ((radius == NULL) | (epsilon == NULL))
+  // check radius and epsilon values -  read readme for the validity of argument. Question: How do we check radius and epsilon.
+  if ((radius < 0) || (epsilon < 0 ))
   {
     printf("Wrong inputs\n");
     return EXIT_FAILURE;
@@ -41,20 +41,25 @@ int main(int argc, char **argv){
 	// check for error in converting to gray scale
    if (gray_image == NULL)
   {
+    BMP_Free(original_image);
+    BMP_Free(gray_image);
     return EXIT_FAILURE;
   }  
 
   // call adaptive threshold function
-  BMPImage *adapted_image = AdaptiveThresholding(gray_image, radius, epsilon); //Check for Error.s
+  BMPImage *adapted_image = AdaptiveThresholding(gray_image, radius, epsilon); //Check for Error.
 
   // check for errors after calling adaptive threshold
-   if (adapted_image == NULL)
+  if (adapted_image == NULL)
   {
+    BMP_Free(original_image);
+    BMP_Free(gray_image);
+    BMP_Free(adapted_image);
     return EXIT_FAILURE;
   } 
 
 	// write the adaptive threshold image to file
-  BMP_Write(output_file_name,adapted_image);
+  BMP_Write(output_file_name,adapted_image); //If file write fails, free memory and return EXIT_FAILURE, done within function.
 
   // free all the images
   BMP_Free(original_image);
