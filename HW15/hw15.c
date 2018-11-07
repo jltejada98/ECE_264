a2557433567ca91a737264a95c67d517f7902604
jltejada98 <tejada@purdue.edu>
2018-11-06 22:51:41 -0500

    HW15 Progress in main.c hw15.c (Tested for Tests 1-4, Memory Errors Present)

--- HW15/hw15.c
+++ HW15/hw15.c
@@ -34,6 +34,22 @@ Node * CreateNode(int val)
 	/*
 	Create a new Node with `value` set to `val`. Set `next` to NULL.
 	*/
+
+  //Extracted from HW12
+  // allocate memory for the new Node*
+  Node *new_node = NULL;
+  new_node = malloc(sizeof(Node));
+
+  // check memory allocation fails
+  if (new_node == NULL)
+  {
+    return NULL;
+  }
+  new_node->value = val;
+  new_node->next = NULL;
+
+  return new_node;
+
 }
 #endif
 
@@ -44,8 +60,34 @@ void LinkedListCreate(Node ** head, char* name)
 	/*
 	Create a linked list from the input file referenced by `name`.
 	*/
+
+  FILE * fptr = fopen(name, "r");
+  if (fptr == NULL) //Check for fopen failure.
+  {
+    return;
+  }
+
+  Node *temp_node = *head; //Temporary array to seek the end of linked list.
+  int value;//Stores the value of the node to be added.
+
+  fseek(fptr, 0, SEEK_CUR); //Seeks current position of file stream.
+
+  while(fscanf(fptr, "%d", &value) == 1) //Remember that fscanf has already scanned first element.
+  {
+    //Modified from HW12.
+    while((temp_node->next) != NULL)
+    {
+      temp_node = temp_node->next;
+    }
+    temp_node->next = CreateNode(value);
+  }
+
+  fclose(fptr);
+
+  return;
 }
 #endif
+
 #ifdef TEST_REMOVED
 //This function will remove repetitions of a linked list value.
 
@@ -59,5 +101,41 @@ void RemoveDuplicate(Node *headRef)
 	To delete a node: we will map the next Node of the previous Node to the Node after the current Node.
 	Print the linked list after all repetitions have been removed. Use the function given to you for priniting.
 	*/
+
+  //Code Modified from HW12
+  Node *checked_value = headRef;
+  Node *temp_node_previous = headRef;
+  Node *temp_node_current = headRef->next;
+
+  //Determine length of linked list for looping.
+  while(checked_value->next != NULL) //Checks all values in Linked List.
+  {
+    while(temp_node_current->next != NULL) //Checks that all values are not 
+    {
+      if (checked_value->value == temp_node_current->value) //If the Checked Value is equal to the Current value, delete the node.
+      {
+        temp_node_previous->next = temp_node_current->next;
+        free(temp_node_current);
+        temp_node_current = temp_node_previous->next;
+      }
+      else
+      {
+        temp_node_current = temp_node_current->next;
+        temp_node_previous = temp_node_previous->next;
+      }
+    }
+    //Check if Last Value is a duplicate
+    if (checked_value->value == temp_node_current->value) //If the Checked Value is equal to the Current value, delete the node.
+    {
+      free(temp_node_current);
+      temp_node_previous->next = NULL;
+    }
+
+    checked_value = checked_value->next; //Check Next Value
+    temp_node_previous = checked_value; //Set previous node to new checked value.
+    temp_node_current = temp_node_previous->next; //Change current node appropriately.
+  }
+
+  return;
 }
 #endif
--- HW15/main.c
+++ HW15/main.c
@@ -11,13 +11,54 @@
 
 int main(int argc, char **argv)
 {
-	// input argv[1] contains the name of the input file.
 	// Check for the right number of input arguments. 
+  if (argc <= 1)
+  {
+    return EXIT_FAILURE;
+  }
+  // input argv[1] contains the name of the input file.
+  FILE *fptr =  fopen(argv[1], "r");
+
+  if (fptr == NULL)
+  {
+    return EXIT_FAILURE;
+  }
 
 	//Initialize a linked list Node and use the file name from input arguments to create a linked list
-	//Remove all duplicates in the linked list created above.
+	int head_value;
+  fscanf(fptr, "%d", &head_value); //Determines the first value of input file.
+
+  //Allocates memory for Node.
+  Node *head_node = NULL;
+  head_node = malloc(sizeof(Node));
+
+  if (head_node == NULL)
+  {
+    return EXIT_FAILURE;
+  }
+
+  head_node->value = head_value;
+  head_node->next = NULL;
+
+  LinkedListCreate(&head_node, argv[1]);
+
+  fclose(fptr);
+
+  //Remove all duplicates in the linked list created above.
+  RemoveDuplicate(head_node);
+
+  //Print List with no duplicate elements.
+  LinkedListPrint(head_node);
 
 	// release memory
+  Node *temp_node;
+  //Code Taken from ListDestroy() in Intermediate C Programming Textbook.
+  while(head_node != NULL)
+  {
+    temp_node = head_node->next;
+    free(head_node);
+    head_node = temp_node;
+  }
 }
 
 #endif
